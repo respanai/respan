e35f446d4cae832fe5e319125d5563988dc41b98
Nightingalelyy <nightingalelyy@outlook.com>
2026-06-13 13:50:37 +0800

    respan-instrumentation-huggingface

---
+++ python-sdks/instrumentations/respan-instrumentation-huggingface/src/respan_instrumentation_huggingface/_constants.py
@@ -0,0 +1,11 @@
+"""Internal constants for Hugging Face instrumentation."""
+
+HUGGINGFACE_INSTRUMENTATION_NAME = "huggingface"
+HUGGINGFACE_GEN_AI_SYSTEM = "huggingface"
+
+TRANSFORMERS_MODULE = "opentelemetry.instrumentation.transformers"
+TRANSFORMERS_SCOPE_NAME = "opentelemetry.instrumentation.transformers"
+TRANSFORMERS_TEXT_GENERATION_SPAN_NAME = "transformers_text_generation_pipeline.call"
+
+ASSISTANT_ROLE = "assistant"
+USER_ROLE = "user"
